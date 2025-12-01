9615413a2bad778520bb44ac0956795546456e44
lucasgls <lucasglsilva7@gmail.com>
2025-12-01 12:05:42 -0300

    first commit

---
+++ src/test/java/com/lucasgabriel/portifolio_api/PortifolioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasgabriel.portifolio_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortifolioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
